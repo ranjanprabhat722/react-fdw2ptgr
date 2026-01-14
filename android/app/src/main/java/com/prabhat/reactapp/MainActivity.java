84cde00a4181851013887a9151d5646627234d34
ranjanprabhat722 <ranjanprabhat722@gmail.com>
2026-01-14 21:26:21 +0530

    commit

---
+++ android/app/src/main/java/com/prabhat/reactapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.prabhat.reactapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.prabhat.reactapp',
+  appName: 'myApp',
+  webDir: 'build'
+};
+
+export default config;
